bdc8cb6d83868f22f24e9d16aeefcfd88472466c
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-04-10 16:23:54 -0400

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { 
     Component,
-    OnInit
+    OnInit,OnDestroy
 } from '@angular/core';
+import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
+
 import { Location } from '@angular/common';
 import { Router,ActivatedRoute }  from '@angular/router';
 import { Set_User,User,Set_User_Access,Set_Group,SignedInUser } from './com_entities/entities';
@@ -36,7 +38,7 @@ import { setTimeout } from 'timers';
             <li *ngIf="isVisible('Admin')" [ngClass]="{'active' : routeStr.includes('/maintenance')}"><a href="#p3" data-toggle="tab" (click)="routeOnly('maintenance')"  class="lnk-maintenance"><i class="fa fa-wrench"></i>&nbsp;Maintenance</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><i class="fa fa-user-circle"></i><span>&nbsp;Hello, {{currentUser.FirstName + ' ' + currentUser.LastName }}!</span></li>
+            <li><a><i class="fa fa-user-circle"></i><span>&nbsp;Hello, {{currentUser.FirstName + ' ' + currentUser.LastName }}!</span></a></li>
           </ul>
         </div>
       </div>
@@ -44,14 +46,38 @@ import { setTimeout } from 'timers';
     <div class="main-body">
       <router-outlet></router-outlet>
     </div>
+    <a [hidden]="true" id="errorModalBtn" data-toggle="modal" href='#modal-id'>Trigger modal</a>
+    <div class="modal fade" id="modal-id" data-backdrop="static" data-keyboard="false">
+      <div class="modal-dialog" style="overflow-y: initial;">
+          <div class="modal-content">
+            <br /><br />
+            <p align="center" style="color: red"><strong>{{idleState}}</strong></p>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" (click)="reloadPage()" data-dismiss="modal">Refresh</button>
+            </div>
+          </div>
+      </div>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnDestroy{
   // currentUser: User= new User('','','','');
   currentUser: User= new User('','','','');
   routeStr:string='';
   private data: any;
+
+  /*
+    IDLE
+    */
+  public idleState = 'Not started.';
+  private timedOut = false;
+  private isIdle=false;
+  private idleEndCount = 0;
+  private idleStartCount = 0;
+  private idleTimeoutCount = 0;
+  private idleTimeoutWarningCount = 0;
+
 
   constructor(
     private curUserSvc: CurrentUserSvc,
@@ -62,6 +88,7 @@ export class AppComponent {
     private route:ActivatedRoute,
     private location: Location,
     private myTokenSvc : MyTokenSvc,
+    private idle: Idle
   ){
     
     this.router.events.debounceTime(1000).subscribe(
@@ -75,6 +102,52 @@ export class AppComponent {
         this.checkIfAuthenticated();
       }
     );
+    this.isIdle = false;
+
+    this.idle.setIdle(2);
+    this.idle.setTimeout(5);
+    this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+    
+    idle.onIdleEnd.subscribe(() => {
+      this.idleEndCount++;
+      this.idleState = 'No longer idle.'
+    });
+    
+    idle.onTimeout.subscribe(() => {
+      this.idleTimeoutCount++;
+      this.idleState = 'Your Session has timed out.';
+      this.timedOut = true;
+      document.getElementById('errorModalBtn').click();
+    });
+    
+    idle.onIdleStart.subscribe(() => {
+      this.idleStartCount++;
+      this.idleState = 'You\'ve gone idle!'
+    });
+    
+    idle.onTimeoutWarning.subscribe((countdown) => {
+     
+      this.idleTimeoutWarningCount++;
+      this.idleState = 'You will time out in ' + countdown + ' seconds!'
+
+    });
+    
+    this.reset();
+  }
+
+  ngOnDestroy() {
+    this.idle.stop();
+  }
+
+  reloadPage(){
+    location.reload();
+  }
+
+  reset() {
+
+    this.idle.watch();
+    this.idleState = 'Started.';
+    this.timedOut = false;
   }
 
   //test
