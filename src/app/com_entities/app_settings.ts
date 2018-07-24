3cfe1f244d245851ddca1d43c65a25558645ac3a
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-07-24 06:52:03 -0400

    up

--- src/app/com_entities/app_settings.ts
+++ src/app/com_entities/app_settings.ts
@@ -1,7 +1,7 @@
 export class AppSettings {
     // public static CURRENT_URL = "http://localhost:60812/api/"; 
-    // public static CURRENT_URL = "https://skillsetclientdev.azurewebsites.net/api/"; 
-    public static CURRENT_URL = "api/"; 
+    public static CURRENT_URL = "https://skillsetclientdev.azurewebsites.net/api/"; 
+    // public static CURRENT_URL = "api/"; 
     // public static BTAM_URL = "http://localhost:49475/api/SingleSignIn/"
     // public static BTAM_URL = "https://btaccessmanagementbw-dev.azurewebsites.net/api/SingleSignIn/"
     public static HANDLEERROR(error: any): Promise<any> {
